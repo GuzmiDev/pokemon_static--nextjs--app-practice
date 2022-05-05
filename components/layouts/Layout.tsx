d514cfc6b35125520b34dda81381094b41952d2b
Erick Guzman <guzmi.dev@gmail.com>
2022-05-04 19:23:11 -0600

    :art: navbar

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head";
+import { NavBar } from "../ui";
 
 interface Props {
   children: ReactNode;
@@ -18,8 +19,15 @@ export const Layout: FC<Props> = ({ children, title }) => {
         />
         <meta name="keywords" content={`${title}, pokémon, pokedex`} />
       </Head>
+      <NavBar />
 
-      <main>{children}</main>
+      <main
+        style={{
+          padding: "0px, 20px",
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,22 @@
-import type { NextPage } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import { Layout } from "../components/layouts";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log({ props });
   return (
     <Layout title="Listado de pokemon">
       <h1>hola</h1>
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log("Hola mundo");
+  return {
+    props: {
+      name: "Erick",
+    },
+  };
 };
 
 export default HomePage;
