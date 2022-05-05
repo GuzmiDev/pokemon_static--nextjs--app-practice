942edd37426204907346d3c7b8f8d76513a0e35e
Erick Guzman <guzmi.dev@gmail.com>
2022-05-04 23:54:35 -0600

    :art: navegación y página de favoritos

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -1,4 +1,5 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
+import NextLink from "next/link";
 import Image from "next/image";
 
 export const NavBar = () => {
@@ -16,22 +17,30 @@ export const NavBar = () => {
         backgroundColor: theme?.colors.gray900.value,
       }}
     >
-      <Image
-        src={
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-        }
-        alt="icono de la app"
-        width={70}
-        height={70}
-      />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okémon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link css={{ alignItems: "center" }}>
+          <Image
+            src={
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+            }
+            alt="icono de la app"
+            width={70}
+            height={70}
+          />
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+      <NextLink href="/favorites" passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components/layouts";
+
+const FavoritesPage = () => {
+  return (
+    <Layout title="Pokémons - Favoritos">
+      <h1>sa</h1>
+    </Layout>
+  );
+};
+
+export default FavoritesPage;
